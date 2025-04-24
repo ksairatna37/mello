70d9a6c6856be3fd85c83bdf1d916baf47e73125
Sairatna Kamble <104418246+ksairatna37@users.noreply.github.com>
2025-04-24 23:39:00 +0530

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { createClient } = require('@supabase/supabase-js');
 const nodemailer = require('nodemailer');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
