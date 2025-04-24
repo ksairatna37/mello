d90de9db51ce9e89ceedfce653a27acae1d1f41f
Sairatna Kamble <104418246+ksairatna37@users.noreply.github.com>
2025-04-24 23:51:05 +0530

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -180,6 +180,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log('Server running on port ${PORT}');
-});
+app.listen(PORT);
